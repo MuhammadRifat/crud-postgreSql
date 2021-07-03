861d6f201be0c79576c5e6d528569dd532723ada
Muhammad Rifat <hrifat450@gmail.com>
2021-07-03 20:34:41 +0600

    crud complete

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ app.listen(5000, ()=>{
 
 client.connect();
 
-app.get('/users', (req, res)=>{
-    client.query(`Select * from users`, (err, result)=>{
+// load user data by email
+app.post('/user', (req, res)=>{
+    client.query(`Select * from users where email='${req.body.email}'`, (err, result)=>{
         if(!err){
             res.send(result.rows);
         }
@@ -22,21 +23,47 @@ app.get('/users', (req, res)=>{
     client.end;
 })
 
-app.get('/user/:id', (req, res)=>{
-    client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
-        if(!err){
-            res.send(result.rows);
-        }
+// check sign in user
+app.post('/signIn', (req, res)=>{
+    const signInQuery = `Select * from users where email='${req.body.email}' AND password='${req.body.password}'`;
+    client.query(signInQuery, (err, result)=>{
+        res.send(result.rows.length > 0);
     });
     client.end;
 })
 
+// insert user data
 app.post('/addUser', (req, res)=> {
     const user = req.body;
-    let insertQuery = `insert into users(email, name, dob, profession) 
-                       values('${user.email}', '${user.name}', '${user.dob}', '${user.profession}')`
+    let insertQuery = `insert into users(email, name, dob, profession, password) 
+                       values('${user.email}', '${user.name}', '${user.dob}', '${user.profession}', '${user.password}')`
 
     client.query(insertQuery, (err, result)=>{
+        res.send(result.rowCount > 0);
+    })
+    client.end;
+})
+
+// delete user
+app.delete('/deleteUser', (req, res)=> {
+    let deleteQuery = `delete from users where email='${req.body.email}'`;
+
+    client.query(deleteQuery, (err, result)=>{
+        res.send(result.rowCount > 0);
+    })
+    client.end;
+})
+
+// update user data by id
+app.put('/updateUser', (req, res)=> {
+    let user = req.body;
+    let updateQuery = `update users
+                       set name = '${user.name}',
+                       dob = '${user.dob}',
+                       profession = '${user.profession}'
+                       where id = ${user.id}`
+
+    client.query(updateQuery, (err, result)=>{
         res.send(result.rowCount > 0);
     })
     client.end;
